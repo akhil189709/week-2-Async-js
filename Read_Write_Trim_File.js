0029bdbc02572523e5e9f350f4eb1e9b98de7d59
AkhilKumar <akhildhiman1897@gmail.com>
2024-12-03 00:53:31 +0530

    updated promises files

--- Read_Write_Trim_File.js
+++ Read_Write_Trim_File.js
@@ -1,3 +1,4 @@
+const { rejects } = require("assert");
 const fs = require("fs");
 
 //Read the file using the promise!
@@ -26,38 +27,46 @@ function writeFile(filePath) {
   });
 }
 writeFile("b.txt").then((content) => {
-  console.log("updated content of b.txt is: "+content);
+  console.log("updated content of b.txt is: " + content);
 });
 
 /// clean the file i.e. remove the extra spaces from the start and from the end in the given file!
 function cleanFile(filePath) {
-  return new Promise((resolve) => {
-    //first we have to read the data from  the file
+  return new Promise((resolve, reject) => {
+    // First, read the data from the file
     fs.readFile(filePath, "utf-8", (err, data) => {
       if (err) {
-        console.log("there is an error here!");
-      } else {
+        console.log("There is an error here!");
+        reject(err); // Reject the promise in case of an error
         return;
       }
-      //now remove the spaces here
-      let trimmedData = data
-        .split("\n") //it splits into lines
-        .map((line) => line.trim()) //trims the space from the each line
-        .join("\n"); //joins the line back
 
-      //now writes back the trimmed data to the file
+      // Now remove the spaces
+      let trimmedData = data
+        .split("\n") // Split into lines
+        .map((line) => line.trim()) // Trim spaces from each line
+        .join("\n"); // Join the lines back
+
+      // Now write back the trimmed data to the file
       fs.writeFile(filePath, trimmedData, (err) => {
         if (err) {
-          console.log("error" + err);
+          console.log("Error:", err);
+          reject(err); // Reject if writing fails
         } else {
-          console.log("Data is trimmed successfullly");
-          resolve(trimmedData);
+          console.log("Data is trimmed successfully");
+          resolve(trimmedData); // Resolve the promise with trimmed data
         }
       });
     });
-   });
+  });
 }
-cleanFile("b.txt").then(function(trimmedData) {
-  console.log("the file reading,writing and the cleaning is done!");
 
-})
+// Call the function
+cleanFile("b.txt")
+  .then(function (trimmedData) {
+    console.log("The file reading, writing, and cleaning is done!");
+    console.log("Trimmed Data:", trimmedData);
+  })
+  .catch(function (err) {
+    console.error("An error occurred:", err);
+  });
